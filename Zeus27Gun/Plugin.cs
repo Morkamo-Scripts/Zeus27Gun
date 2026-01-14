3dacadbd8f8e9752267873a29ed4ebd524f9e31c
Кирилл <131317714+Morkamo@users.noreply.github.com>
2026-01-14 21:09:10 +0900

    Initial commit

---
+++ Zeus27Gun/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using LabApi.Events;
+
+namespace Zeus27Gun
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Zeus27";
+        public override string Prefix => Name;
+        public override string Author => "Morkamo";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 12, 1);
+
+        public static Plugin Instance { get; private set; }
+        
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
